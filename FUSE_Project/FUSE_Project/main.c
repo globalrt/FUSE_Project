07fae6174bdfdb4538508c1c82569a0270e9c190
Dongsung Kim <kiding@me.com>
2015-11-27 18:10:50 +0900

    Added mandatory macros.

--- FUSE_Project/FUSE_Project/main.c
+++ FUSE_Project/FUSE_Project/main.c
@@ -7,15 +7,16 @@
 #define FUSE_USE_VERSION 29
 #include "fuse.h" // DEBUG
 
+#define BLOCK_SIZE_KB   4     // 블록 크기 (KB)
+#define VOLUME_SIZE_MB  100   // 파일 시스템 볼륨 크기 (MB)
+#define MAX_FILENAME    255   // 최대 파일 이름 길이
+#define INODE_SIZE_BYTE 512   // 각 inode당 메모리 크기
+
 typedef struct inode inode;
-
-
-
 struct inode {
-
     struct stat attr;
     
-    uint8_t name[256];
+    uint8_t name[MAX_FILENAME + 1];
     
     inode *parent;
     inode *leftSibling;
