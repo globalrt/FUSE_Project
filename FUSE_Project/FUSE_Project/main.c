9ed86417783d0e4b69e5a7b97e2f67d5379c3e4f
JIHAN LEE <globalrt@naver.com>
2015-11-27 16:47:40 +0900

    in ode make

--- FUSE_Project/FUSE_Project/main.c
+++ FUSE_Project/FUSE_Project/main.c
@@ -1,4 +1,31 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+
+
+typedef struct inode inode;
+
+
+
+struct inode {
+
+    struct stat attr;
+    
+    uint8_t name[256];
+    
+    inode *parent;
+    inode *leftSibling;
+    inode *rightSibling;
+    inode *firstChild;
+    inode *lastChild;
+    
+    void *data;
+};
+
+static inode root;
+
+
 
 int main(int argc, const char * argv[]) {
     return 1;
