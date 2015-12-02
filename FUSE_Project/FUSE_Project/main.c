dbbac14fc7d3e8e8106234b8b23edf568694381e
Dongsung Kim <kiding@me.com>
2015-12-02 19:20:11 +0900

    Re-arranged the include and define section for clarity and compatibility with Makefile. Now that "fuse.h" is changed to <fuse.h>, Header Search Paths is added in the Xcode project.

--- FUSE_Project/FUSE_Project/main.c
+++ FUSE_Project/FUSE_Project/main.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/statvfs.h>
-
-#define FUSE_USE_VERSION 29
-#include "fuse.h" // DEBUG
-
+#define FUSE_USE_VERSION 29   // 사용할 FUSE API 버전
 #define BLOCK_SIZE_KB   4     // 블록 크기 (KB)
 #define VOLUME_SIZE_MB  100   // 파일 시스템 볼륨 크기 (MB)
 #define MAX_FILENAME    255   // 최대 파일 이름 길이
 #define INODE_SIZE_BYTE 512   // 각 inode당 메모리 크기
+
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <errno.h>
 
 typedef struct inode inode;
 struct inode {
