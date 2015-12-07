1aea84c30cf769731c9c9d093490fb7f9cd9ce5c
Dongsung Kim <kiding@me.com>
2015-12-07 18:48:26 +0900

    All the comments.

--- FUSE_Project/FUSE_Project/asdfs.h
+++ FUSE_Project/FUSE_Project/asdfs.h
@@ -34,14 +34,14 @@ int asdfs_readdir (const char *path, void *buf, fuse_fill_dir_t filer, off_t off
 // 파일 생성
 int asdfs_mknod (const char *path, mode_t mode, dev_t rdev);
 
+// 생성 및 수정 시간 변경
+int asdfs_utimens (const char *path, const struct timespec tv[2]);
+
 // 파일 삭제
 int asdfs_unlink (const char *path);
 
 // 파일 열기
 int asdfs_open (const char *path, struct fuse_file_info *fi);
-
-// 생성 및 수정 시간 변경
-int asdfs_utimens (const char *path, const struct timespec tv[2]);
 
 // 파일 읽기
 int asdfs_read (const char *path, char *mem, size_t size, off_t off, struct fuse_file_info *fi);
@@ -60,28 +60,5 @@ int asdfs_chown (const char *path, uid_t uid, gid_t gid);
 
 // 파일 이동
 int asdfs_rename (const char *oldpath, const char *newpath);
-
-static struct fuse_operations asdfs_oper = {
-    .init     = asdfs_init,
-    .statfs   = asdfs_statfs,
-    .getattr  = asdfs_getattr,
-
-    .mkdir    = asdfs_mkdir,
-    .rmdir    = asdfs_rmdir,
-    .opendir  = asdfs_opendir,
-    .readdir  = asdfs_readdir,
-
-    .mknod    = asdfs_mknod,
-    .unlink   = asdfs_unlink,
-    .open     = asdfs_open,
-    .utimens  = asdfs_utimens,
-    .read     = asdfs_read,
-    .truncate = asdfs_truncate,
-    .write    = asdfs_write,
-
-    .chmod    = asdfs_chmod,
-    .chown    = asdfs_chown,   
-    .rename   = asdfs_rename, 
-};
 
 #endif
--- FUSE_Project/FUSE_Project/main.c
+++ FUSE_Project/FUSE_Project/main.c
@@ -1,6 +1,31 @@
 #include "asdfs.h"
 #include <fuse.h>
 
+static struct fuse_operations asdfs_oper = {
+    .init     = asdfs_init,     // 파일 시스템 초기화
+    .statfs   = asdfs_statfs,   // 파일 시스템 정보 조회
+    .getattr  = asdfs_getattr,  // 파일 정보 조회
+
+    .mkdir    = asdfs_mkdir,    // 디렉터리 생성
+    .rmdir    = asdfs_rmdir,    // 디렉터리 삭제
+    .opendir  = asdfs_opendir,  // 디렉터리 열기
+    .readdir  = asdfs_readdir,  // 디렉터리 읽기
+
+    .mknod    = asdfs_mknod,    // 파일 생성
+    .utimens  = asdfs_utimens,  // 생성 및 수정 시간 변경
+    .unlink   = asdfs_unlink,   // 파일 삭제
+
+    .open     = asdfs_open,     // 파일 열기
+    .read     = asdfs_read,     // 파일 읽기
+    .truncate = asdfs_truncate, // 이미 있는 파일 크기 변경
+    .write    = asdfs_write,    // 파일 쓰기
+
+    .chmod    = asdfs_chmod,    // 파일 권한 변경
+    .chown    = asdfs_chown,    // 파일 소유자 변경
+    .rename   = asdfs_rename,   // 파일 이동
+};
+
 int main(int argc, char *argv[]) {
+    // fuse 파일 시스템 시작
     return fuse_main(argc, argv, &asdfs_oper, NULL);
 }
